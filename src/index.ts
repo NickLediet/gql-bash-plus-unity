97e883fa8521f3b3114b89735fb76830a88b78f6
Nicholas Lediet <nicklediet@gmail.com>
2024-12-19 00:55:45 -0500

    Setup basic CGI server with script being called

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
 
-const message: string = "Hello World!"
+import cgi from 'cgi'
+import { createServer } from 'node:http'
+import path from 'path'
+import { cwd } from 'process'
 
-console.log(message)
+// @TODO: migrate to yaml config w/defauts
+const PORT = process.env.PORT || 8888
+
+// @TODO: migrate to yaml config w/defauts
+const script = path.resolve('src/router.cgi')
+
+console.log(script)
+
+const server = createServer(
+  cgi(script)
+)
+
+
+// @TODO: replace with an actual logger
+server.on('error', console.error)
+
+
+server.listen(PORT, () => {
+  console.log(`Starting server on port ${PORT}`)
+})
---
+++ src/router.cgi
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Headers are written first. The special "Status"
+# header indicates the response status code
+echo "Status: 200"
+echo "Content-Type: text/plain"
+echo
+echo "Hello World!"
